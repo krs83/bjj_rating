5de9ee68e44b2b09158205debdfd0aa7bc6d8fe3
krs83 <krs83@yandex.ru>
2025-09-15 22:21:03 +0300

    creating athletes and tournaments sqlmodels

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,10 +1,5 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from backend.app.database_settings import db_settings
-from sqlalchemy.orm import DeclarativeBase
 
 engine = create_async_engine(db_settings.DB_URL)
 async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
-
-
-class Base(DeclarativeBase):
-    pass
