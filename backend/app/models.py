549b39b8cf404caf4f9df53a95f534324a5641b1
krs83 <krs83@yandex.ru>
2025-09-21 20:13:44 +0300

    first models inits

---
+++ backend/app/models.py
@@ -0,0 +1,27 @@
+from datetime import date
+
+from sqlmodel import Field, SQLModel, String, Date, Relationship, Column
+
+
+class Athlete(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    place: int = Field(nullable=False)
+    fullname: str = Field(String(50),index=True, nullable=False)
+    birth: date = Field(sa_column=Column(Date))
+    city: str = Field(String(50),index=True)
+    region: str = Field(String(50),index=True, nullable=False)
+    points: int = Field(default=0, ge=0)
+
+    tournaments: list["Tournament"] = Relationship(back_populates='athletes',
+                                                   link_model='AthleteTournamentLink')
+
+
+class Tournament(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    title: str = Field(String(50),index=True, nullable=False)
+    smoothcomp_id: int = Field(nullable=False)
+    smoothcomp_date: date = Field(sa_column=Column(Date))
+
+    athletes: list[Athlete] = Relationship(back_populates='tournaments',
+                                             link_model='AthleteTournamentLink')
+
