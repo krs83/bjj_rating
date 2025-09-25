e78b68362db833966022e12f796e0750f56b3c90
krs83 <krs83@yandex.ru>
2025-09-25 22:25:15 +0300

    update read and create crud func

--- backend/src/crud.py
+++ backend/src/crud.py
@@ -9,20 +9,30 @@ async def get_athletes(db: DPSes, offset: int, limit: int):
     stmt = select(Athlete).offset(offset).limit(limit)
     res = await db.exec(stmt)
     athletes = res.all()
-    athletes = [AthleteResponse.model_validate(athlete) for athlete in athletes]
     return athletes
 
 async def get_athlete(db: DPSes, athlete_id: int):
-    stmt = select(Athlete).where(Athlete.id == athlete_id)
-    res = await db.exec(stmt)
-    athlete = res.one()
+    athlete = await db.get(Athlete, athlete_id)
+    # TODO: Проверка на наличие id - if not athlete: Exception
     return athlete
 
 async def create_athlete(db: DPSes, athlete_data: AthleteAdd):
     athlete = await find_existing_athlete(db, athlete_data)
     db.add(athlete)
-    await db.commit()
+    await db.commit() # TODO: оптимизировать запросы. Пока 2
     await db.refresh(athlete)
     await calculating_place(db)
     await db.commit()
     return athlete_data
+
+# async def part_update_athlete(db: DPSes, athlete_id:int, athlete_data: AthleteAdd, unset=True):
+#     stmt = select(Athlete).where(Athlete.id == athlete_id)
+#     await db.exec(stmt)
+#     db.add(athlete_data.model_dump(exclude_unset=unset))
+#     await db.commit()
+#     return athlete_data
+#
+# async def full_update_athlete(db: DPSes,  athlete_id:int, athlete_data: AthleteAdd, unset=False):
+#     db.add(athlete_data.model_dump(exclude_unset=unset))
+#     await db.commit()
+#     return athlete_data
