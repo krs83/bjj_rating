7291daacdd792a1c1ee10838167ad35fe9158e29
krs83 <krs83@yandex.ru>
2025-09-25 22:23:39 +0300

    update api get and post

--- backend/src/api.py
+++ backend/src/api.py
@@ -1,20 +1,24 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 
 from backend.src.crud import get_athlete, get_athletes, create_athlete
 from backend.src.dependencies import DPSes
-from backend.src.models import AthleteAdd
+from backend.src.models import AthleteAdd, AthleteResponse, AthleteBase
 
 r_athletes = APIRouter(prefix='/athletes', tags=['Спортсмены'])
 
 # Athletes endpoints
-@r_athletes.get('')
-async def get_all_athletes(db: DPSes, offset: int = 0, limit: int = 50):
+@r_athletes.get('', response_model=list[AthleteResponse])
+async def get_all_athletes(db: DPSes, offset: int = 0, limit: int = Query(default=50, le=100)) -> AthleteBase:
     return await get_athletes(db, offset, limit)
 
-@r_athletes.get('/athlete_id')
-async def get_one_athlete(db: DPSes, athlete_id: int):
+@r_athletes.get('/athlete_id', response_model=AthleteResponse)
+async def get_one_athlete(db: DPSes, athlete_id: int) -> AthleteBase:
     return await get_athlete(db, athlete_id)
 
-@r_athletes.post('')
-async def add_athlete(db: DPSes, athlete_data: AthleteAdd):
+@r_athletes.post('', response_model=AthleteResponse)
+async def add_athlete(db: DPSes, athlete_data: AthleteAdd) -> AthleteBase:
     return await create_athlete(db, athlete_data)
+
+# @r_athletes.patch('/athlete_id')
+# async def update_athlete():
+#     return await part_update_athlete(db, )
