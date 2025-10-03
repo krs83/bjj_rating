43f9d8578c06d69681c6b6d01835707a2d16f32d
krs83 <krs83@yandex.ru>
2025-10-03 15:00:54 +0300

    add admin DP and HTTPExceptions

--- backend/src/dependencies.py
+++ backend/src/dependencies.py
@@ -1,10 +1,13 @@
-from fastapi import Depends
+from fastapi import Depends, HTTPException, status
 from typing import Annotated
 import jwt
 
 from fastapi.security import OAuth2PasswordBearer
+from jwt import InvalidTokenError
+from pydantic import ValidationError
 from sqlmodel import select
 from sqlmodel.ext.asyncio.session import AsyncSession
+
 
 from backend.src.config import settings
 from backend.src.database import engine
@@ -23,15 +26,30 @@ async def get_session() -> AsyncSession:
 DPSes = Annotated[AsyncSession, Depends(get_session)]
 
 async def get_current_user(db: DPSes, token: DPToken):
-    print(f'{token=}')
-    payload = jwt.decode(
-        token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
-    )
-    token_data = TokenData(**payload)
-    stmt = select(User).where(User.email == token_data.sub )
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
+        token_data = TokenData(**payload)
+    except (InvalidTokenError, ValidationError):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Could not validate credentials",
+        )
+    stmt = select(User).where(User.email == token_data.sub)
     res = await db.exec(stmt)
-    user = res.first()
-    print(f'{user=}')
+    user = res.scalar_one_or_none()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    if not user.is_active:
+        raise HTTPException(status_code=400, detail="Inactive user")
     return user
 
 CurrentUser = Annotated[User, Depends(get_current_user)]
+
+def get_current_active_superuser(current_user: CurrentUser) -> User:
+    if not current_user.is_superuser:
+        raise HTTPException(
+            status_code=403, detail="The user doesn't have enough privileges"
+        )
+    return current_user
