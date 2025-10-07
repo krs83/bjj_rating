2b6d95dcb5909784b22c37c5b7b84bf8cc43c744
krs83 <krs83@yandex.ru>
2025-10-07 13:55:49 +0300

    update user models - add boolean is_admin field

--- backend/src/models/token.py
+++ backend/src/models/token.py
@@ -3,7 +3,7 @@ from sqlmodel import SQLModel
 
 class Token(SQLModel):
     access_token: str
-    token_type: str
+    token_type: str = 'Bearer'
 
 
 class TokenData(SQLModel):
--- backend/src/models/user.py
+++ backend/src/models/user.py
@@ -1,32 +1,39 @@
-from typing import Literal
 from sqlmodel import Field, SQLModel
 from pydantic import EmailStr
 
-ROLES = Literal[1,2,3,4]
-
-USER: ROLES = 1
-COACH: ROLES = 2
-ADMIN: ROLES = 3
-SUPER_ADMIN: ROLES = 4
 
 class UserBase(SQLModel):
-    email: EmailStr = Field(nullable=False, unique=True)
+    email: EmailStr = Field(nullable=False, index=True, unique=True, max_length=255)
+    is_admin: bool = Field(default=False)
 
-class User(UserBase, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    hashed_password: str = Field(max_length=200)
-    role: int = Field(default=USER, nullable=False)
 
-class UserAdd(UserBase):
-    password: str = Field(min_length=3)
+class UserCreate(UserBase):
+    password: str = Field(min_length=6, max_length=40)
+
 
 class UserUpdate(UserBase):
     email: EmailStr | None = None
     password: str | None = None
 
+
+class UserUpdateMe(SQLModel):
+    email: EmailStr | None = None
+
+
+class UserUpdatePassword(SQLModel):
+    current_password: str = Field(min_length=6, max_length=40)
+    new_password: str = Field(min_length=6, max_length=40)
+
+
 class UserLogin(SQLModel):
-    email: str
+    email: EmailStr
     password: str
+
 
 class UserResponse(UserBase):
     id: int
+
+
+class User(UserBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    hashed_password: str = Field(max_length=200)
