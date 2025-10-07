e5b9eaca6677cff94629d41bb38875a263bbc115
krs83 <krs83@yandex.ru>
2025-10-07 09:01:25 +0300

    add returned values to funcs

--- backend/src/security.py
+++ backend/src/security.py
@@ -12,11 +12,11 @@ pwd_context = CryptContext(
 )
 
 
-def hash_password(password: str):
+def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
 
-def verify_password(plain_password, hashed_password):
+def verify_password(plain_password, hashed_password) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
 
