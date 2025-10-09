b25d1a607937665c0246469fb08ca3d78898ed91
krs83 <krs83@yandex.ru>
2025-10-09 10:37:09 +0300

    admin errors handling implementation

--- backend/src/admin/auth.py
+++ backend/src/admin/auth.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.requests import Request
 from starlette.exceptions import HTTPException
 from jwt.exceptions import ExpiredSignatureError
-from starlette.responses import RedirectResponse
+from starlette.responses import RedirectResponse, Response
 
 from backend.src.admin.dependency import session_maker_admin
 from backend.src.config import settings
@@ -23,31 +23,42 @@ async def authenticate_admin(db: AsyncSession, email: str, password: str):
         valid_password = verify_password(password, user.hashed_password)
         if valid_password:
             return user
-        return None
-    return None
+        else:
+            return False
+    else:
+        raise HTTPException(status_code=403, detail="Доступ запрещен")
+
 
 class AdminAuth(AuthenticationBackend):
+    def __init__(self, secret_key: str):
+        super().__init__(secret_key)
+        self.redirect_url = "http://localhost:8000/admin/login"  #TODO: change domain
+
+
     async def login(self, request: Request) -> bool:
 
         form = await request.form()
         email, password = form["username"], form["password"]
 
         async with session_maker_admin() as db:
-            user = await authenticate_admin(db, email, password)
+
+            try:
+                user = await authenticate_admin(db, email, password)
+            except AttributeError:
+                return False
 
             if user:
                 token = create_access_token({'sub': user.email})
                 request.session.update({'token': token})
                 return True
-            return False
 
+            return False
 
     async def logout(self, request: Request) -> bool:
         request.session.clear()
         return True
 
-    async def check_token_exp(self, token: str, request: Request):
-        redirect_url = "http://localhost:8000/admin/login" #TODO: change domain
+    async def check_token_exp(self, token: str, request: Request) -> RedirectResponse:
         if token:
             try:
                 jwt.decode(
@@ -55,8 +66,7 @@ class AdminAuth(AuthenticationBackend):
                 )
             except ExpiredSignatureError:
                 await self.logout(request)
-                return RedirectResponse(redirect_url, status_code=303)
-
+                return RedirectResponse(self.redirect_url, status_code=303)
 
     async def authenticate(self, request: Request) -> bool:
         token = request.session.get("token")
@@ -65,5 +75,3 @@ class AdminAuth(AuthenticationBackend):
         if not token:
             return False
         return True
-
-
