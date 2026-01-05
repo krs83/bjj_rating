61f3f539828d6a0ef76db6eff65d8c97a7d8d01d
krs83 <krs83@yandex.ru>
2026-01-05 21:18:35 +0300

    refactor: сменил название на ATHLETETOURNAMENTLINKEXISTSTEXT

--- backend/src/exceptions/athlete_tournament_link.py
+++ backend/src/exceptions/athlete_tournament_link.py
@@ -2,7 +2,7 @@ from backend.src.exceptions.base import ConflictException
 
 
 class AthleteTournamentLinkIntegrityException(ConflictException):
-    ATHLETETOURNAMENTLINKNOTFOUNDTEXT = "ID турнира(ов) уже существует в БД. Пожалуйста, проверьте!"
+    ATHLETETOURNAMENTLINKEXISTSTEXT = "ID турнира(ов) уже существует в БД. Пожалуйста, проверьте!"
 
     def __init__(self):
         super().__init__(detail=self.ATHLETETOURNAMENTLINKNOTFOUNDTEXT)
